ef3b3e6fd763f5d0b72f8d93f44f24f9ac5615ee
munseunggyu <84954439+munseunggyu@users.noreply.github.com>
2024-07-14 22:02:22 +0900

    Feat: PodcastCard 컴포넌트 추가

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
+
 export default function Home() {
-  return <div className='text-white-1'>hi</div>;
+  return (
+    <div className='mt-9 flex flex-col gap-9 md:overflow-hidden'>
+      <section className='flex flex-col gap-5'>
+        <h1 className='text-20 font-bold text-white-1'>Trending Podcasts</h1>
+
+        <div className='podcast_grid'>
+          {podcastData.map(podcast => (
+            <PodcastCard
+              key={podcast.id}
+              imgUrl={podcast.imgURL}
+              title={podcast.title}
+              description={podcast.description}
+              podcastId={podcast.id}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
---
+++ types/index.ts
@@ -0,0 +1,10 @@
+export interface LatestPodcastCardProps {
+  imgUrl: string;
+  title: string;
+  duration: string;
+  index: number;
+  audioUrl: string;
+  author: string;
+  views: number;
+  podcastId: number;
+}
